5b3d4b4d3695f88df0c36d772c4871f17230fd9c
Marcin Brzeziński <cintrzyk@gmail.com>
2015-03-01 17:45:32 +0100

    breadcrumbs

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,8 +2,12 @@ import Ember from 'ember';
 /* global _ */
 
 export default Ember.Controller.extend({
+  breadCrumb: "<i class='fa fa-dashboard'></i>Home",
+  breadCrumbPath: 'index',
+
   contentHeader: function() {
     var header = this.currentPath.split('.')[0];
     return header !== 'index' ? _.capitalize(header) : 'Dashboard';
   }.property('currentPath')
+
 });
